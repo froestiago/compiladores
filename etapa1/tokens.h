c74653c0e2b5bb0a0df5324a44c9c0de75f5de30
Tiago Fróes <tiagofroes@MacBook-Air-de-Tiago.local>
2023-05-23 19:49:03 -0300

    scanner ta avancando...

---
+++ etapa1/tokens.h
@@ -0,0 +1,27 @@
+/*
+Lista dos tokens, com valores constantes associados. Os valores das
+constantes são arbitrários, mas evite alterá-los.  Cada valor
+deve ser fora de valores possíveis em ASCII pois estes serão usados
+para os caracteres especiais.
+*/
+
+#define TK_PR_INT          301 // done
+#define TK_PR_FLOAT        302 // done
+#define TK_PR_BOOL         303 // done
+#define TK_PR_IF           304 // done
+#define TK_PR_ELSE         305 // done
+#define TK_PR_WHILE        306 // done
+#define TK_PR_RETURN       307 // done
+#define TK_OC_LE           308 // done
+#define TK_OC_GE           309 // done
+#define TK_OC_EQ           310 // done
+#define TK_OC_NE           311 // done
+#define TK_OC_AND          312 // done
+#define TK_OC_OR           313 // done
+#define TK_OC_MAP          314 // done
+#define TK_IDENTIFICADOR   315
+#define TK_LIT_INT         316 // done
+#define TK_LIT_FLOAT       317 // done
+#define TK_LIT_FALSE       318 // done
+#define TK_LIT_TRUE        319 // done
+#define TK_ERRO            320 // done
